591186898c41cfd782aff986d7a9c5e3b4ac31da
Gediminas Šaltenis <gediminas@saltenis.lt>
2021-06-15 01:01:40 +0300

    trigger and read all sensors

--- include/main.h
+++ include/main.h
@@ -2,27 +2,31 @@
 #define main_h
 
 #include <Arduino.h>
-#include <NewPing.h>
 
-#define MAX_DISTANCE 150 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define SONAR_ROUNDTRIP_CM_US 57
+#define SONAR_MAX_DISTANCE 150 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define SONAR_MAX_WAIT 8607 // (SONAR_MAX_DISTANCE + 1) * SONAR_ROUNDTRIP_CM_US
+#define SONAR_PING_OVERHEAD 1
+#define SENSOR_DELAY 5800
 #define SONAR_NUM 3
+#define SONAR_TRIGGER 32
 
-#define MIDI_DISTANCE_START 10
-#define MIDI_DISTANCE_END 100
+#define MIDI_DISTANCE_START 570 // 10CM * SONAR_ROUNDTRIP_CM_US
+#define MIDI_DISTANCE_END 7125 // 125CM * SONAR_ROUNDTRIP_CM_US
 
 #define MIDI_VALUE_MIN 0
 #define MIDI_VALUE_MAX 127
 
-NewPing sonar[SONAR_NUM] = {
-    NewPing(34 /* TRIGGER */, 35 /* ECHO */, MAX_DISTANCE),
-    NewPing(32 /* TRIGGER */, 33 /* ECHO */, MAX_DISTANCE),
-    NewPing(25 /* TRIGGER */, 26 /* ECHO */, MAX_DISTANCE),
+uint8_t sonar[SONAR_NUM] = {
+    35,
+    33,
+    26,
 };
-
-int16_t sonarDistance[SONAR_NUM];
-int16_t sonarDistanceTotal[SONAR_NUM];
-int16_t sonarDistanceAvg[SONAR_NUM][10];
-int16_t sonarDistanceAvgIndex[SONAR_NUM];
+uint32_t sonarDistanceNew[SONAR_NUM];
+uint32_t sonarDistance[SONAR_NUM];
+uint32_t sonarDistanceTotal[SONAR_NUM];
+uint32_t sonarDistanceAvg[SONAR_NUM][10];
+uint8_t sonarDistanceAvgIndex[SONAR_NUM];
 
 int16_t sonarMidi[SONAR_NUM];
 bool sonarMidiOnOff[SONAR_NUM] = {
@@ -33,5 +37,10 @@ bool sonarMidiOnOff[SONAR_NUM] = {
 
 void setup();
 void loop();
+void triggerAll();
+void readSonars();
+void calculateMovingAverage();
+void printDebug();
+uint32_t convertToCm(uint32_t us);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 
 void setup() {
+    memset(sonarDistanceNew, 0, sizeof(sonarDistance));
     memset(sonarDistance, 0, sizeof(sonarDistance));
     memset(sonarDistanceTotal, 0, sizeof(sonarDistanceTotal));
     memset(sonarDistanceAvg, 0, sizeof(sonarDistanceAvg));
@@ -8,23 +9,21 @@ void setup() {
 
     memset(sonarMidi, 0, sizeof(sonarMidi));
 
+    pinMode(SONAR_TRIGGER, OUTPUT);
+    for (uint8_t i = 0; i < SONAR_NUM; i++) {
+        pinMode(sonar[i], INPUT);
+    }
+
     Serial.begin(115200);
 }
 
 void loop() {
-    // Moving avarage
-    for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.
-        delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+    // Just delay to slow down while inside
+    delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
 
-        int16_t pingResult = sonar[i].ping_cm();
-        sonarDistanceTotal[i] -= sonarDistanceAvg[i][sonarDistanceAvgIndex[i]];
-        sonarDistanceAvg[i][sonarDistanceAvgIndex[i]] = pingResult;
-        sonarDistanceTotal[i] += pingResult;
+    readSonars();
 
-        sonarDistanceAvgIndex[i] = (sonarDistanceAvgIndex[i] + 1) % 10;
-
-        sonarDistance[i] = sonarDistanceTotal[i] / 10.0f;
-    }
+    calculateMovingAverage();
 
     // Convert to MIDI values
     for (uint8_t i = 0; i < SONAR_NUM; i++) {
@@ -51,16 +50,80 @@ void loop() {
 
     // Process MIDI here
 
-    // Debug
+    printDebug();
+}
+
+void triggerAll() {
+    digitalWrite(SONAR_TRIGGER, LOW);   // Set the trigger pin low, should already be low, but this will make sure it is.
+    delayMicroseconds(4);         // Wait for pin to go low.
+    digitalWrite(SONAR_TRIGGER, HIGH);  // Set trigger pin high, this tells the sensor to send out a ping.
+    delayMicroseconds(10);        // Wait long enough for the sensor to realize the trigger pin is high. Sensor specs say to wait 10uS.
+    digitalWrite(SONAR_TRIGGER, LOW);   // Set trigger pin back to low.
+
+    // Delay until at least one sensor start doing anything
+    uint32_t startMaxDelay = micros() + SONAR_MAX_WAIT + SENSOR_DELAY; // Maximum time we'll wait for ping to start (most sensors are <450uS, the SRF06 can take up to 34,300uS!)
+    while (micros() < startMaxDelay) {
+        for (uint8_t i = 0; i < SONAR_NUM; i++) {
+            if (digitalRead(sonar[i]) == 1) {
+                return;
+            }
+        }
+    }
+}
+
+void readSonars() {
+    bool finished = false;
+
+    // Reset new values
+    for (uint8_t i = 0; i < SONAR_NUM; i++) { sonarDistanceNew[i] = 0; }
+
+    triggerAll();
+
+    uint32_t startMicros = micros();
+    uint32_t timeoutMicros = startMicros + SONAR_MAX_WAIT;
+
+    while (micros() < timeoutMicros && finished == false) {
+        finished = true;
+        for (uint8_t i = 0; i < SONAR_NUM; i++) {
+            if (sonarDistanceNew[i] == 0) {
+                if (digitalRead(sonar[i]) == 0) {
+                    sonarDistanceNew[i] = micros() - startMicros - SONAR_PING_OVERHEAD;
+                } else {
+                    finished = false;
+                }
+            }
+        }
+    }
+}
+
+void calculateMovingAverage() {
+    for (uint8_t i = 0; i < SONAR_NUM; i++) {
+        int16_t pingResult = sonarDistanceNew[i];
+        sonarDistanceTotal[i] -= sonarDistanceAvg[i][sonarDistanceAvgIndex[i]];
+        sonarDistanceAvg[i][sonarDistanceAvgIndex[i]] = pingResult;
+        sonarDistanceTotal[i] += pingResult;
+
+        sonarDistanceAvgIndex[i] = (sonarDistanceAvgIndex[i] + 1) % 10;
+
+        sonarDistance[i] = sonarDistanceTotal[i] / 10.0f;
+    }
+}
+
+void printDebug() {
     Serial.print("Sensors: ");
     for (uint8_t i = 0; i < SONAR_NUM; i++) {
         Serial.print(i); Serial.print(" (");
 
-        Serial.print("Distance: "); Serial.print(sonarDistance[i]); Serial.print("cm ");
+        Serial.print("RAW: "); Serial.print(sonarDistance[i]); Serial.print(" ");
+        Serial.print("Distance: "); Serial.print(convertToCm(sonarDistance[i])); Serial.print("cm ");
         Serial.print("MIDI: "); Serial.print(sonarMidi[i]); Serial.print(" ");
         Serial.print("MIDI on/off: "); Serial.print(sonarMidiOnOff[i]);
 
         Serial.print(") ");
     }
     Serial.println();
+}
+
+uint32_t convertToCm(uint32_t us) {
+    return us / SONAR_ROUNDTRIP_CM_US;
 }
