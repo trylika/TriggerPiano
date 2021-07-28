365d0bb4acab62cd34b8a8e1d4c8ef392b1c26cd
Gediminas Šaltenis <gediminas@saltenis.lt>
2021-07-29 00:34:03 +0300

    increase read distance

--- include/main.h
+++ include/main.h
@@ -4,15 +4,15 @@
 #include <Arduino.h>
 
 #define SONAR_ROUNDTRIP_CM_US 57
-#define SONAR_MAX_DISTANCE 150 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-#define SONAR_MAX_WAIT 8607 // (SONAR_MAX_DISTANCE + 1) * SONAR_ROUNDTRIP_CM_US
+#define SONAR_MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define SONAR_MAX_WAIT 17157 // (SONAR_MAX_DISTANCE + 1) * SONAR_ROUNDTRIP_CM_US
 #define SONAR_PING_OVERHEAD 2
 #define SONAR_SENSOR_DELAY 5800
 #define SONAR_NUM 3
 #define SONAR_TRIGGER 32
 
 #define MIDI_DISTANCE_START 570 // 10CM * SONAR_ROUNDTRIP_CM_US
-#define MIDI_DISTANCE_END 7125 // 125CM * SONAR_ROUNDTRIP_CM_US
+#define MIDI_DISTANCE_END 11400 // 125CM * SONAR_ROUNDTRIP_CM_US
 
 #define MIDI_VALUE_MIN 0
 #define MIDI_VALUE_MAX 127
